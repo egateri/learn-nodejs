3f6789697ae1a17805999a745ac6a655f82d8e57
Eliud Gateri <egateri@gmail.com>
2023-05-04 15:44:10 +0300

    Added MD5 Encryption

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 
+const {md5} = require('../public/js/md5.js');
 const express = require("express");
 
 const router = express.Router();
@@ -20,10 +21,11 @@ router.get("/login", (req, res) => {
 router.post("/login", (req, res) => {
 
   // var isGetMethod = false;
-
+ var name = req.body.name;
+ var password =md5(req.body.password);
  var details = {
-      name: req.body.name,
-      password: req.body.password,
+      name: name,
+      password: password
     };
 
 
